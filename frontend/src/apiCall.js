cd3c393fcb2a6277b86b6c5bb20e88e699e2b151
KibetBrian <briankibet2040@gmail.com>
2022-12-19 15:11:15 +0300

    add api calls

--- frontend/src/apiCall.js
+++ frontend/src/apiCall.js
@@ -1,40 +1,28 @@
-import axios  from "axios";
-const axiosInstance = axios.create(
-    {
-        baseUrl: process.env.REACT_APP_API_URL
-    }
-)
+import axiosInstance from "./axios";
 
-export const loginCall = async (userCredentials, dispatch) => 
-{
-    dispatch({type: "LOGIN_START"});
-    try 
-    {
+export const loginCall = async (userCredentials, dispatch) => {
+    dispatch({ type: "LOGIN_START" });
+    try {
         const res = await axiosInstance.post('auth/login', userCredentials);
-        dispatch({type: "LOGIN_SUCCESS", payload: res.data})
-        console.log(res.data)
-        localStorage.setItem('user', JSON.stringify(res.data)); 
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+        localStorage.setItem('user', JSON.stringify(res.data.userData.user));
     }
-    catch(err)
-    {
-        dispatch({type: "LOGIN_FAILURE", payload: err})
-    }
-}
-
-export const registrationCall = async (traderCredentials, dispatch) =>
-{
-    dispatch ({type: "REGISTRATION_START)"})
-
-    try 
-    {
-        const res = await axiosInstance.post ('auth/register', traderCredentials);
-        dispatch({type: "REGISTRATION_SUCCESS", payload: res.data})
-
-    }
-    catch (err)
-    {
-        dispatch ({type: "REGISTRATION_FAILED", payload: err})
+    catch (err) {
+        console.log("Error from loggin in", err)
+        dispatch({ type: "LOGIN_FAILURE", payload: err })
     }
 }
 
- 
+export const registrationCall = async (traderCredentials, dispatch) => {
+    dispatch({ type: "REGISTRATION_START)" })
+
+    try {
+        const res = await axiosInstance.post('auth/register', traderCredentials);
+        dispatch({ type: "REGISTRATION_SUCCESS", payload: res.data })
+
+    }
+    catch (err) {
+        dispatch({ type: "REGISTRATION_FAILED", payload: err })
+    }
+}
+
