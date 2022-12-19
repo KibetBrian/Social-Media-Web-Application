189d77149c625a0fbad98fb746ff3951bc6980c9
KibetBrian <briankibet2040@gmail.com>
2022-12-19 13:27:46 +0300

    update routes

--- backend/index.js
+++ backend/index.js
@@ -1,86 +1,72 @@
-const express = require ('express');
-const mongoose = require ('mongoose');
-const morgan = require ('morgan');
-const dotenv = require ('dotenv');
-const helmet = require ('helmet');
-const userRoute = require ('./routes/users');
-const authRoute = require ('./routes/auth');
-const postRoute = require ('./routes/posts');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 const conversationRoute = require('./routes/conversations');
-const messageRoute = require ('./routes/messages');
+const messageRoute = require('./routes/messages');
 const categoryRoute = require('./routes/categories');
 const forgotPasswordRoute = require('./routes/forgotPassword')
-const multer = require ('multer');
-const path = require ('path');
+const multer = require('multer');
+const path = require('path');
 const jwt = require('jsonwebtoken');
 const { connect } = require('http2');
 const cors = require('cors');
 
-
-
 const app = express();
+const PORT = process.env.PORT_NUMBER || 8080;
 dotenv.config();
 
 //Database Connection
-mongoose.connect(process.env.mongo_url).then(res=>{
-   if(res.connections)
-   {
-       console.log('Database connected')
-   }
-   else 
-   {
-       console.log(res)
-   }
-})
-//Cors
-app.use(cors());
-
-
-
+mongoose.connect(process.env.MONGO_URI).then(res => {
+    if (res.connections) {
+        console.log('Database connected')
+    } else {
+        console.log("Database connection failed. Error", res)
+    }
+});
 
 //Multer //Image Upload
 const storage = multer.diskStorage({
-    destination: (req, file, callback) =>
-    {
+    destination: (req, file, callback) => {
         callback(null, 'images/assets')
     },
-    filename: (req, file, callback) =>
-    {
+    filename: (req, file, callback) => {
         callback(null, req.body.name)
     }
 });
 
-const upload = multer ({storage: storage});
-app.post('/api/upload', upload.single('file'), (req, res)=>
-{
-    try
-    {
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    try {
         return res.status(200).json("Image Uploaded")
     }
-    catch(err)
-    {
+    catch (err) {
+        res.status(500).json("Internal server error");
         console.log(err)
     }
 })
 
 //middlewares
 app.use(express.json());
+app.use(cors());
 app.use(helmet());
 app.use(morgan('common'));
-
 
 app.use("/assets", express.static(path.join(__dirname, "images/assets")))
 
 //Routes
-app.use('/api/users', userRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/posts', postRoute);
-app.use('/api/categories', categoryRoute);
-app.use('/api/conversation', conversationRoute);
-app.use('/api/messages', messageRoute);
-app.use('/api/forgot-password', forgotPasswordRoute);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/posts', postRoute);
+app.use('/api/v1/categories', categoryRoute);
+app.use('/api/v1/conversation', conversationRoute);
+app.use('/api/v1/messages', messageRoute);
+app.use('/api/v1/forgot-password', forgotPasswordRoute);
 
-app.listen(process.env.PORT_NUMBER, 8080, ()=>
-{
+app.listen(PORT, () => {
     console.log("Server started");
 })
